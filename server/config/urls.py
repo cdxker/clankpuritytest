7bbe839e90d7776d4089a2e0fd41555f5417d04b
cdxker <fdenzell@gmail.com>
2026-05-26 11:10:21 -0700

    refactor server layout

--- server/config/urls.py
+++ server/config/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from clankpuritytest.views import home
+from server.clankpuritytest.views import home
 
 urlpatterns = [
     path('', home, name='home'),
